417db4cde31070fa2c345eeb6a3b13fa3956cf82
Suraj Vitthal Patil <145219543+SurajPatil2645@users.noreply.github.com>
2025-02-04 19:02:47 +0530

    Time: 0 ms (100%), Space: 95 MB (93.81%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> merged;
-        int i = 0, j = 0;
-        
-        // Merge two sorted arrays
-        while (i < nums1.size() && j < nums2.size()) {
-            if (nums1[i] < nums2[j]) {
-                merged.push_back(nums1[i++]);
+        if (nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2, nums1);  // Ensure nums1 is the smaller array
+        }
+
+        int x = nums1.size(), y = nums2.size();
+        int low = 0, high = x;
+
+        while (low <= high) {
+            int partitionX = (low + high) / 2;
+            int partitionY = (x + y + 1) / 2 - partitionX;
+
+            int maxLeftX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
+            int minRightX = (partitionX == x) ? INT_MAX : nums1[partitionX];
+
+            int maxLeftY = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
+            int minRightY = (partitionY == y) ? INT_MAX : nums2[partitionY];
+
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                if ((x + y) % 2 == 0) {
+                    return (max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2.0;
+                } else {
+                    return max(maxLeftX, maxLeftY);
+                }
+            } else if (maxLeftX > minRightY) {
+                high = partitionX - 1;
             } else {
-                merged.push_back(nums2[j++]);
+                low = partitionX + 1;
             }
         }
-        
-        // Add remaining elements from nums1
-        while (i < nums1.size()) {
-            merged.push_back(nums1[i++]);
-        }
-        
-        // Add remaining elements from nums2
-        while (j < nums2.size()) {
-            merged.push_back(nums2[j++]);
-        }
-        
-        int totalSize = merged.size();
-        
-        // Find the median
-        if (totalSize % 2 == 1) {
-            return merged[totalSize / 2];  // Odd case
-        } else {
-            return (merged[totalSize / 2 - 1] + merged[totalSize / 2]) / 2.0;  // Even case
-        }
+
+        throw invalid_argument("Input arrays are not sorted correctly.");
     }
 };
